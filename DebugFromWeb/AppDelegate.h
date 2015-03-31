218ec9d7fc96e95bc04eaa2830d84acd7b62eb73
Kiran <kiran@Administrators-Mac-mini.local>
2015-03-31 16:09:26 +0530

    Initial Commit

---
+++ DebugFromWeb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DebugFromWeb
+//
+//  Created by Kiran on 31/03/15.
+//  Copyright (c) 2015 Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DebugFromWeb/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DebugFromWeb
+//
+//  Created by Kiran on 31/03/15.
+//  Copyright (c) 2015 Kiran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
